17aabbe1e9a1c5eae4b625b70fd026e986bb4f94
pmestre-Forge <p.mestre@live.com.pt>
2026-03-25 15:35:33 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='rotfeed-v1';
+const ASSETS=['/brainrot-app/','/brainrot-app/index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
